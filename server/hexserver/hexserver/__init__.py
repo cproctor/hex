472329308c6fbb9c7a9b2537d78c55aaf4f00cb7
Chris Proctor <chris.proctor@gmail.com>
2013-12-04 19:00:19 -0800

    Updating static view declaration

--- server/hexserver/hexserver/__init__.py
+++ server/hexserver/hexserver/__init__.py
@@ -7,7 +7,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_renderer('json', JSON(indent=4))
 
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'hexserver:static', cache_max_age=3600)
 
     config.add_route('landing', '/')
     config.add_route('show_current_spell', 'spells/current')
